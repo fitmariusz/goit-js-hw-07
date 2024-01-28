323625ad32a7b8e23b94d082de24f4dc18de78b4
fitmariusz <fitmariusz@gmail.com>
2024-01-28 21:56:12 +0100

    finish firrst  gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,16 +1,20 @@
 import { galleryItems } from './gallery-items.js';
-// import * as basicLightbox from 'basiclightbox'
 
+function addCssFile() {
 
+    var link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href="https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.css"
+    document.getElementsByTagName("head")[0].appendChild(link);
+   
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = "https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js";
+    document.getElementsByTagName("body")[0].appendChild(script);
+}
 
-
-
-
-// Change code below this line
-
+addCssFile()
 const ulGallery = document.querySelector(".gallery");
-
-console.log(ulGallery);
 
 galleryItems.forEach((item) => {
     // // const ulImage=getElementsByClassName('gallery')
@@ -20,9 +24,9 @@ galleryItems.forEach((item) => {
     aImage.setAttribute('class', 'gallery__link');
     const image = document.createElement('img');
     image.setAttribute('class', 'gallery__image');
-    image.setAttribute('src', item.preview);
-    image.setAttribute('alt', item.description);
-    image.setAttribute('href', item.original);
+    image.src=item.preview;
+    image.alt=item.description;
+    image.dataset.href=item.original;
     aImage.append(image);
     divBoxImage.append(aImage);
     ulGallery.append(divBoxImage);
@@ -30,77 +34,19 @@ galleryItems.forEach((item) => {
 
 
 ulGallery.addEventListener("click", (event) => {
-  event.preventDefault();
+    event.preventDefault();
+    
+    const lightbox = basicLightbox.create(`
+    <img src="${event.target.dataset.href}" width="800" height="600">
+    `);
 
-  const lightbox = basicLightbox.create(`
-    <img src="${event.target.href}" width="800" height="600">
-  `);
+    lightbox.show();
 
-  lightbox.show();
+    document.addEventListener("keydown", event => {
+        if (event.key === 'Escape') {
+            lightbox.close();
+        }
+    });
 });
 
 
-// ulGallery.addEventListener("click", clickImage);
-
-// function clickImage(event) {
-
-//     console.log('test');
-    
-
-// // const instance = basicLightbox.create(`
-// //     <img src="assets/images/image.png" width="800" height="600">
-// // `)
-
-// // instance.show()
-// }
-
-
-
-
-
-
-
-
-
-
-// const createElement = document.querySelector('[data-create]');
-// const destroyElement = document.querySelector('[data-destroy]');
-// const divBox = document.getElementById("boxes");
-// const valueInput = document.querySelector('input');
-// valueInput.value = 1;
-
-
-// function createBox()
-// {
-//   const startSize = 30;
-//   const stepSize = 10;
-//   for (let i = 0; i < valueInput.value; i++) {
-//     const box = document.createElement("div");
-//     const randomColor = getRandomHexColor();
-
-//     box.style.backgroundColor = randomColor;
-//     box.setAttribute('class', `divBox`);
-//     box.style.height = `${startSize + startSize * i}px`;
-//     box.style.width = `${startSize + stepSize * i}px`;
-//     divBox.append(box);
-//   }
-// }
-
-// function destroyBox() {
-//   const divs = document.querySelectorAll(".divBox");
-//   divs.forEach(div => { 
-//     div.remove();
-//   });
-// }
-
-// createElement.addEventListener("click", () => {
-//   destroyBox();
-//   createBox();
-
-// });
- 
-// destroyElement.addEventListener("click", () => {
-//   destroyBox();
-// });
-
-console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,51 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+function addCssFile() {
 
-console.log(galleryItems);
+    var link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href="https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.css"
+    document.getElementsByTagName("head")[0].appendChild(link);
+   
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = "https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js";
+    document.getElementsByTagName("body")[0].appendChild(script);
+}
+
+addCssFile()
+const ulGallery = document.querySelector(".gallery");
+
+galleryItems.forEach((item) => {
+    // // const ulImage=getElementsByClassName('gallery')
+    const divBoxImage = document.createElement("div");
+    divBoxImage.setAttribute('class', 'gallery__item');
+    const aImage = document.createElement('a');
+    aImage.setAttribute('class', 'gallery__link');
+    const image = document.createElement('img');
+    image.setAttribute('class', 'gallery__image');
+    image.src=item.preview;
+    image.alt=item.description;
+    image.dataset.href=item.original;
+    aImage.append(image);
+    divBoxImage.append(aImage);
+    ulGallery.append(divBoxImage);
+});
+
+
+ulGallery.addEventListener("click", (event) => {
+    event.preventDefault();
+    
+    const lightbox = basicLightbox.create(`
+    <img src="${event.target.dataset.href}" width="800" height="600">
+    `);
+
+    lightbox.show();
+
+    document.addEventListener("keydown", event => {
+        if (event.key === 'Escape') {
+            lightbox.close();
+        }
+    });
+});
+
