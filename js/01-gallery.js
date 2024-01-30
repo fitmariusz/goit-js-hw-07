e86c822cfbc634d7019293dcfe9962ff5b8c9b6f
fitmariusz <fitmariusz@gmail.com>
2024-01-30 02:38:07 +0100

    chamge in js gallery 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,20 +2,7 @@
 
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-function addJsFile() {
 
-    var link = document.createElement("link");
-    link.rel = "stylesheet";
-    link.href="https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.css"
-    document.getElementsByTagName("head")[0].appendChild(link);
-   
-    var script = document.createElement('script');
-    script.async = true;
-    script.src = "https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js";
-    document.getElementsByTagName("body")[0].appendChild(script);
-}
-
-addJsFile()
 const ulGallery = document.querySelector(".gallery");
 
 const arrayDiv=galleryItems.map((item) => {
@@ -28,10 +15,9 @@ const arrayDiv=galleryItems.map((item) => {
     image.setAttribute('class', 'gallery__image');
     image.src=item.preview;
     image.alt=item.description;
-    image.dataset.href=item.original;
+    image.dataset.source=item.original;
     aImage.append(image);
     divBoxImage.append(aImage);
-    console.log(divBoxImage);
     return divBoxImage;
 });
 ulGallery.append(...arrayDiv);
@@ -40,15 +26,20 @@ ulGallery.addEventListener("click", (event) => {
     event.preventDefault();
     
     const lightbox = basicLightbox.create(`
-    <img src="${event.target.dataset.href}" width="800" height="600">
-    `);
-
+    <img src="${event.target.dataset.source}" width="800" height="600">
+    `, {
+        onShow: (lightbox) => { document.addEventListener("keydown",keyEsc);  },
+    });
+    
     lightbox.show();
 
-    document.addEventListener("keydown", event => {
-        if (event.key === 'Escape') {
-            lightbox.close();
+    function keyEsc(event) {
+    if (event.key === 'Escape') {
+        lightbox.close();
         }
-    });
+    }
 });
+
+
+
 
